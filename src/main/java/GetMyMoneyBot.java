3aa053d50052ba394c364e546d0861294f095a91
Xenia <xenia-ev2502@yandex.ru>
2020-11-02 17:46:54 +0500

    echo bot2

--- src/main/java/GetMyMoneyBot.java
+++ src/main/java/GetMyMoneyBot.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-public class getMyMoneyBot {
+public class GetMyMoneyBot extends TelegramLongPollingBot {
+
+    public void onUpdateReceived(Update update) {
+
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            // Set variables
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+
+            SendMessage message = new SendMessage() // Create a message object object
+                    .setChatId(chat_id)
+                    .setText(message_text);
+            try {
+                execute(message); // Sending our message object to user
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getBotUsername() {
+        // TODO
+        return "getPoorStudentsMoneyBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        // TODO
+        return "1371432192:AAHo_acUpWsEfA8v9_xKq40hR0JRIY6kmfM";
+    }
 }
