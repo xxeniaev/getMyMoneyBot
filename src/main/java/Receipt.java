08687a89860a207142d1ec767493a8e1246745dd
donilNick <dengorbachev0@gmail.com>
2020-12-07 15:33:24 +0500

    new dialog

--- src/main/java/Receipt.java
+++ src/main/java/Receipt.java
@@ -1,6 +1,8 @@
 import com.fasterxml.jackson.databind.JsonNode;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -36,5 +38,22 @@ public class Receipt{
         s.append("------------------------------------\n").append("Итог: ")
                 .append(this.receiptData.data.get("totalSum").asDouble()/100).append(" рублей");
         return s.toString();
+    }
+
+    public ArrayList<String[]> giveElementsReceipt()
+    {
+        List<JsonNode> jsonNodesItems = this.receiptData.getJsonNodeItems();
+        ArrayList<String[]> arrayList = new ArrayList<>();
+        int j = 1;
+        for (JsonNode jsonNodesItem : jsonNodesItems) {
+            String[] s = new String[4];
+            s[0] = jsonNodesItem.get("name").asText().replaceFirst("([0-9:*]+)", j+". ");
+            s[1] = String.valueOf((jsonNodesItem.get("price").asDouble() / 100));
+            s[2] = jsonNodesItem.get("quantity").asText();
+            s[3] = String.valueOf((jsonNodesItem.get("sum").asDouble() / 100));
+            arrayList.add(s);
+            j++;
+        }
+        return arrayList;
     }
 }
